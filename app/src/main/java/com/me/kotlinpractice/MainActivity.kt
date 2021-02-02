9c8bbd4edc7ab62d5c7743f5cd417f30dc1e1885
Administrator <2997328272@qq.com>
2021-02-02 15:56:28 +0800

    倒计时功能完善

--- app/src/main/java/com/me/kotlinpractice/MainActivity.kt
+++ app/src/main/java/com/me/kotlinpractice/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.me.kotlinpractice
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
 import com.blankj.utilcode.util.LogUtils
 import com.me.kotlinpractice.databinding.ActivityMainBinding
 import com.me.library.ext.countDownCoroutines
@@ -21,133 +23,25 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.tvCount.setOnClickListener {
-            job = countTime(0,60,{
-                print("倒计时:${it}s")
-            },{
-                print("倒计时:结束")
-            })
+            startActivity(Intent(this,SecondActivity::class.java))
+            finish()
         }
         binding.btnFinish.setOnClickListener {
-            LogUtils.d("倒计时:开始>>>>")
+            LogUtils.d("倒计时:开始>>>> ")
             if (job != null){
                 job?.cancel()
                 job = null
                 return@setOnClickListener
             }
-//            main()
-//            counter(Dispatchers.Unconfined,0,5,1000,{
-//                LogUtils.d("倒计时>>>$it")
-//                binding.tvCount.text = "倒计时:${it}s"
-//            },{
-//                LogUtils.d("倒计时>>>完成")
-//            })
-//            GlobalScope.launch (Dispatchers.Main){
-//                delay(600)
-//                binding.tvCount.text = "调整>>>>"
-//            }
-            job = countDownCoroutines(12,{
+            job = countDownCoroutines(5,{
                 binding.tvCount.text = "倒计时:${it}s"
             },{
                 binding.tvCount.text = "结束"
-            })
-//           job = flow{
-//                   for (i in 10 downTo 0){
-//                       emit(i)
-//                       LogUtils.d("emit on ${Thread.currentThread().name}")
-//                       delay(1000)
-//                   }
-//               }.flowOn(Dispatchers.Default)
-//                   .onCompletion {
-//                           print("倒计时:结束")
-//                           LogUtils.d("倒计时:结束>>>>")
-//                       }
-//                       .onEach {
-//                           runOnUiThread {
-//                               LogUtils.d("倒计时:${it}s>>>>")
-//                               binding.tvCount.text = "倒计时:${it}s"
-//                           }
-//                           GlobalScope.launch (Dispatchers.Main){
-//                               LogUtils.d("倒计时:${it}s>>>>")
-//                               binding.tvCount.text = "倒计时:${it}s"
-//                           }
-//                           LogUtils.d("倒计时:${it}s>>>>")
-//                           binding.tvCount.text = "倒计时:${it}s"
-//
-//                       }.flowOn(Dispatchers.Main)
-//                   .launchIn(GlobalScope)
+            },lifecycleScope)
 
-        }
-
-//        flow<Int> { emit(0) }.flowOn(Dispatchers.Default)
-    }
-
-    fun countTime(start:Int,end:Int,onProgress:(Int)->Unit,onFinish:()->Unit): Job {
-        return GlobalScope.launch {
-            flow {
-                for (i in start..end){
-                    emit(i)
-                    delay(1000)
-                }
-            }.onEach {
-                onProgress.invoke(it) }
-            .onCompletion { onFinish.invoke() }
-
-        }
-    }
-
-//    fun main() = GlobalScope.launch(Dispatchers.IO) {
-//        flow {
-//            for (i in 1..5) {
-//                emit(i)
-//                LogUtils.d("emit on ${Thread.currentThread().name}")
-//                delay(1000)
-//            }
-//        }.flowOn(Dispatchers.IO)
-//                .collect {
-////                    binding.tvCount.text = "倒计时:${it}s"
-//                    println("${Thread.currentThread().name}: $it")
-//                    LogUtils.d("${Thread.currentThread().name}: $it")
-//                }
-//    }
-
-    fun simple(): Flow<Int> = flow {
-        for (i in 1..3) {
-//            Thread.sleep(100) // 假装我们以消耗 CPU 的方式进行计算
-            delay(100)
-            LogUtils.d("Emitting $i")
-            emit(i) // 发射下一个值
-        }
-    }.flowOn(Dispatchers.IO) // 在流构建器中改变消耗 CPU 代码上下文的正确方式
-
-    fun main() = GlobalScope.launch(Dispatchers.Main) {
-        simple()
-                .catch {
-                    LogUtils.d(it.message)
-                }
-                .collect { value ->
-            LogUtils.d("Collected $value")
-        }
-    }
-
-    fun counter(dispatcher: CoroutineContext, start:Int, end:Int, delay:Long,
-                onProgress:((value:Int)->Unit), onFinish: (()->Unit)?= null){
-        val out = flow<Int> {
-            for (i in start..end) {
-                emit(i)
-                delay(delay)
-            }
-        }
-        GlobalScope.launch {
-            withContext(dispatcher) {
-                out.collect {
-                    onProgress.invoke(it)
-                }
-                onFinish?.invoke()
-            }
         }
     }
 
--- app/src/test/java/com/me/kotlinpractice/ExampleUnitTest.kt
+++ app/src/test/java/com/me/kotlinpractice/ExampleUnitTest.kt
@@ -17,28 +17,8 @@ import org.junit.Assert.*
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
-//        assertEquals(4, 2 + 2)
+        assertEquals(4, 2 + 2)
 //        val user = fromJson<List<User>>("""[{"name":"张三","sex":2},{"name":"李四","sex":2}]""")
 //        print(user.toString())
-//        countTime(0,60,{
-//            print("倒计时:${it}s")
-//        },{
-//            print("倒计时:结束")
-//        })
-    }
-
-    fun countTime(start:Int,end:Int,onProgress:(Int)->Unit,onFinish:()->Unit): Job {
-        return GlobalScope.launch {
-            flow {
-                for (i in start..end){
-                    emit(i)
-                    delay(1000)
-                }
-            }.onEach {
-
-                 }
-                .onCompletion { onFinish.invoke() }
-
-        }
     }
 }
